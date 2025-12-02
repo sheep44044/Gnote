3d199c94d0b45eebf1a23d775fcd5a802c7ea13e
WangZhonghao <3290120053@qq.com>
2025-12-02 21:20:03 +0800

    给note结构体添加了IsPrivate字段

--- internal/models/note.go
+++ internal/models/note.go
@@ -8,6 +8,7 @@ type Note struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
 	Title     string    `json:"title" binding:"required"`
 	Content   string    `json:"content" binding:"required"`
+	IsPrivate bool      `gorm:"default:false" json:"is_private"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 	Tags      []Tag     `gorm:"many2many:note_tags;"`
