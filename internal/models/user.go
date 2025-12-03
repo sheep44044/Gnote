1b96b170a96630727d44df644fe28a52b5d9b174
WangZhonghao <3290120053@qq.com>
2025-12-03 12:49:07 +0800

    修改了user结构体，添加了avatar和bio应用于之后的personalpage

--- internal/models/user.go
+++ internal/models/user.go
@@ -7,5 +7,7 @@ import (
 type User struct {
 	gorm.Model
 	Username string `gorm:"unique;not null;size:50"`
-	Password string `gorm:"not null;size:255"` // bcrypt hash
+	Password string `gorm:"not null;size:255"`
+	Avatar   string `gorm:"size:255" json:"avatar,omitempty"`
+	Bio      string `gorm:"type:text" json:"bio,omitempty"`
 }
