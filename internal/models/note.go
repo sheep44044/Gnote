f4ab6264ac306f0431de719975984a677dff9c9a
WangZhonghao <3290120053@qq.com>
2025-12-07 21:23:51 +0800

    删去了IsFavorite字段，增加了FavoriteCount字段

--- internal/models/note.go
+++ internal/models/note.go
@@ -5,14 +5,17 @@ import (
 )
 
 type Note struct {
-	ID         uint      `json:"id" gorm:"primaryKey"`
-	UserID     uint      `gorm:"index"`
-	Title      string    `json:"title" binding:"required"`
-	Content    string    `json:"content" binding:"required"`
-	IsPrivate  bool      `gorm:"default:false" json:"is_private"`
-	IsPinned   bool      `gorm:"default:false;index"` // 是否置顶
-	IsFavorite bool      `gorm:"default:false;index"`
-	CreatedAt  time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt  time.Time `json:"updated_at" gorm:"autoUpdateTime"`
-	Tags       []Tag     `gorm:"many2many:note_tags;"`
+	ID      uint   `json:"id" gorm:"primaryKey"`
+	UserID  uint   `gorm:"index"`
+	Title   string `json:"title" binding:"required"`
+	Content string `json:"content" binding:"required"`
+
+	IsPrivate     bool `gorm:"default:false" json:"is_private"`
+	IsPinned      bool `gorm:"default:false;index"` // 是否置顶
+	IsFavorite    bool `gorm:"default:false;index"`
+	FavoriteCount int  `gorm:"default:0"` // 收藏总数
+
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	Tags      []Tag     `gorm:"many2many:note_tags;"`
 }
