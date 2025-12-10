31b9aebaff4f2e6a2ebd99ac6de35557729560a0
WangZhonghao <3290120053@qq.com>
2025-12-10 17:05:44 +0800

    添加FollowCount和FanCount字段，为关注他人打下伏笔

--- internal/models/user.go
+++ internal/models/user.go
@@ -10,4 +10,7 @@ type User struct {
 	Password string `gorm:"not null;size:255"`
 	Avatar   string `gorm:"size:255" json:"avatar,omitempty"`
 	Bio      string `gorm:"type:text" json:"bio,omitempty"`
+
+	FollowCount int `gorm:"default:0" json:"follow_count,omitempty"`
+	FanCount    int `gorm:"default:0" json:"fan_count,omitempty"`
 }
