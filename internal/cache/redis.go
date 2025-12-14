839a5a6a8305260adc6c946ca6c7e073f7fe08b9
WangZhonghao <3290120053@qq.com>
2025-12-14 15:00:33 +0800

    废弃全局变量的方法，改为注入依赖注入

--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -11,34 +11,31 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-var (
-	Rdb *redis.Client
-	ctx = context.Background()
-)
+type RedisCache struct {
+	client *redis.Client
+}
 
-func Init(cfg *config.Config) error {
-	// 创建Redis客户端
-	Rdb = redis.NewClient(&redis.Options{
+func New(cfg *config.Config) (*RedisCache, error) {
+	rdb := redis.NewClient(&redis.Options{
 		Addr:     fmt.Sprintf("%s:%s", cfg.RedisHost, cfg.RedisPort),
 		Password: cfg.RedisPassword,
 		DB:       cfg.RedisDB,
 	})
 
-	// 测试连接
-	_, err := Rdb.Ping(ctx).Result()
+	ctx := context.Background()
+	_, err := rdb.Ping(ctx).Result()
 	if err != nil {
-		return fmt.Errorf("failed to connect to Redis: %w", err)
+		return nil, fmt.Errorf("failed to connect to Redis: %w", err)
 	}
 
-	return nil
+	return &RedisCache{client: rdb}, nil
 }
 
-// 设置键值对，带过期时间
-func Set(key string, value interface{}, expiration time.Duration) error {
-	return Rdb.Set(ctx, key, value, expiration).Err()
+func (c *RedisCache) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	return c.client.Set(ctx, key, value, expiration).Err()
 }
 
-func SetWithRandomTTL(key string, value interface{}, baseTTL time.Duration) error {
+func (c *RedisCache) SetWithRandomTTL(ctx context.Context, key string, value interface{}, baseTTL time.Duration) error {
 	// 在基础TTL上增加±10%的随机浮动
 	jitter := time.Duration(rand.Int63n(int64(baseTTL/5)) - int64(baseTTL/10))
 	actualTTL := baseTTL + jitter
@@ -48,45 +45,41 @@ func SetWithRandomTTL(key string, value interface{}, baseTTL time.Duration) erro
 		actualTTL = baseTTL
 	}
 
-	return Rdb.Set(ctx, key, value, actualTTL).Err()
+	return c.client.Set(ctx, key, value, actualTTL).Err()
 }
 
-// 获取键值
-func Get(key string) (string, error) {
-	return Rdb.Get(ctx, key).Result()
+func (c *RedisCache) Get(ctx context.Context, key string) (string, error) {
+	return c.client.Get(ctx, key).Result()
 }
 
-// 删除键
-func Del(key string) error {
-	return Rdb.Del(ctx, key).Err()
+func (c *RedisCache) Del(ctx context.Context, key string) error {
+	return c.client.Del(ctx, key).Err()
 }
 
-// 设置哈希
-func HSet(key string, values ...interface{}) error {
-	return Rdb.HSet(ctx, key, values...).Err()
+func (c *RedisCache) HSet(ctx context.Context, key string, values ...interface{}) error {
+	return c.client.HSet(ctx, key, values...).Err()
 }
 
-// 获取哈希
-func HGet(key, field string) (string, error) {
-	return Rdb.HGet(ctx, key, field).Result()
+func (c *RedisCache) HGet(ctx context.Context, key, field string) (string, error) {
+	return c.client.HGet(ctx, key, field).Result()
 }
 
-func ZAdd(key string, members ...redis.Z) (int64, error) {
-	return Rdb.ZAdd(ctx, key, members...).Result()
+func (c *RedisCache) ZAdd(ctx context.Context, key string, members ...redis.Z) (int64, error) {
+	return c.client.ZAdd(ctx, key, members...).Result()
 }
 
-func ZRemRangeByRank(key string, start, stop int64) (int64, error) {
-	return Rdb.ZRemRangeByRank(ctx, key, start, stop).Result()
+func (c *RedisCache) ZRemRangeByRank(ctx context.Context, key string, start, stop int64) (int64, error) {
+	return c.client.ZRemRangeByRank(ctx, key, start, stop).Result()
 }
 
-func ZRem(key string, members ...interface{}) (int64, error) {
-	return Rdb.ZRem(ctx, key, members...).Result()
+func (c *RedisCache) ZRem(ctx context.Context, key string, members ...interface{}) (int64, error) {
+	return c.client.ZRem(ctx, key, members...).Result()
 }
 
-func ZRevRange(key string, start, stop int64) ([]string, error) {
-	return Rdb.ZRevRange(ctx, key, start, stop).Result()
+func (c *RedisCache) ZRevRange(ctx context.Context, key string, start, stop int64) ([]string, error) {
+	return c.client.ZRevRange(ctx, key, start, stop).Result()
 }
 
-func Expire(key string, expiration time.Duration) (bool, error) {
-	return Rdb.Expire(ctx, key, expiration).Result()
+func (c *RedisCache) Expire(ctx context.Context, key string, expiration time.Duration) (bool, error) {
+	return c.client.Expire(ctx, key, expiration).Result()
 }
