cf0377e6094346fea85ff3addff56623645d07e5
WangZhonghao <3290120053@qq.com>
2026-01-24 17:00:33 +0800

    更新

--- internal/note/note_handler.go
+++ internal/note/note_handler.go
@@ -1,8 +1,10 @@
 package note
 
 import (
+	"note/internal/ai"
 	"note/internal/cache"
 	"note/internal/mq"
+	"note/internal/vector"
 
 	"gorm.io/gorm"
 )
@@ -11,8 +13,10 @@ type NoteHandler struct {
 	db     *gorm.DB
 	cache  *cache.RedisCache
 	rabbit *mq.RabbitMQ
+	ai     *ai.AIService
+	qdrant *vector.QdrantService
 }
 
-func NewNoteHandler(db *gorm.DB, cache *cache.RedisCache, rabbitMQ *mq.RabbitMQ) *NoteHandler {
-	return &NoteHandler{db: db, cache: cache, rabbit: rabbitMQ}
+func NewNoteHandler(db *gorm.DB, cache *cache.RedisCache, rabbitMQ *mq.RabbitMQ, ai *ai.AIService, qdrant *vector.QdrantService) *NoteHandler {
+	return &NoteHandler{db: db, cache: cache, rabbit: rabbitMQ, ai: ai, qdrant: qdrant}
 }
